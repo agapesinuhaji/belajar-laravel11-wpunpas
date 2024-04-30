5ab20135b5af2bbdd425ad8ed90eb5113ada5db0
priahitam <agapemanase27@gmail.com>
2024-04-30 22:00:52 +0700

    Belajar Laravel 11 | 3. Struktur Folder

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about', [
+        'nama' => 'Agape Manase Sinuhaji',
+    ]);
+});
+
+// Tugas
+// Buat 2 Route Baru
+// 1. /blog
+// 2 buah artikel, judul dan isi
+// 2. /contact
+// email, social media
