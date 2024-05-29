e2ab83656be70103d979b438e6861b3460647f50
priahitam <agapemanase27@gmail.com>
2024-05-29 11:48:31 +0700

    Belajar Laravel 11 | 5. Blade Component

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home Page']);
 });
 
 Route::get('/about', function () {
     return view('about', [
         'nama' => 'Agape Manase Sinuhaji',
+        'title' => 'About'
     ]);
 });
 
@@ -20,14 +21,9 @@ Route::get('/about', function () {
 // email, social media
 
 Route::get('/blog', function () {
-    return view('blog', [
-        'judul1' => 'Blog Pertama',
-        'isi1' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi explicabo ipsum, dolores eligendi optio atque odio aspernatur, minima ratione distinctio temporibus? Neque deserunt accusantium laboriosam expedita provident placeat veniam soluta!', 
-        'judul2' => 'Blog Kedua',
-        'isi2' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi explicabo ipsum, dolores eligendi optio atque odio aspernatur, minima ratione distinctio temporibus? Neque deserunt accusantium laboriosam expedita provident placeat veniam soluta!',  
-    ]);
+    return view('blog', ['title' => 'Blog']);
 });
 
 Route::get('/contact', function() {
-    return view('contact');
+    return view('contact', ['title' => 'Contact']);
 });
