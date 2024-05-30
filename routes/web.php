e1ed728b881dc7ca5ddb988ed765acdbd2c4a6ee
priahitam <agapemanase27@gmail.com>
2024-05-30 22:18:41 +0700

    Belajar Laravel 11 - | 6. View Data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -20,8 +21,49 @@ Route::get('/about', function () {
 // 2. /contact
 // email, social media
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog','posts' =>[
+        [
+            'id' => '1',
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Agape Manase Sinuhaji',
+            'body'=> 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae aspernatur dolores, labore cum amet repellat minus, sed eveniet illo id illum qui ex enim optio aliquid suscipit provident pariatur cupiditate.',
+        ],
+        [
+            'id' => '2',
+            'title' => 'Judul Artikel 2',
+            'slug' => 'judul-artikel-2',
+            'author' => 'Agape Manase Sinuhaji',
+            'body'=> 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae aspernatur dolores, labore cum amet repellat minus, sed eveniet illo id illum qui ex enim optio aliquid suscipit provident pariatur cupiditate. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae aspernatur dolores, labore cum amet repellat minus, sed eveniet illo id illum qui ex enim optio aliquid suscipit provident pariatur cupiditate.',
+        ]
+    ]]);
+});
+
+Route::get('posts/{slug}', function($slug) {
+    $posts = [
+        [
+            'id' => '1',
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Agape Manase Sinuhaji',
+            'body'=> 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae aspernatur dolores, labore cum amet repellat minus, sed eveniet illo id illum qui ex enim optio aliquid suscipit provident pariatur cupiditate.',
+        ],
+        [
+            'id' => '2',
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Agape Manase Sinuhaji',
+            'body'=> 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae aspernatur dolores, labore cum amet repellat minus, sed eveniet illo id illum qui ex enim optio aliquid suscipit provident pariatur cupiditate. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae aspernatur dolores, labore cum amet repellat minus, sed eveniet illo id illum qui ex enim optio aliquid suscipit provident pariatur cupiditate.',
+        ],
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+    
 });
 
 Route::get('/contact', function() {
