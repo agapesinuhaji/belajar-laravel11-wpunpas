d5b9c39ce8319ae45bb245efb8f40e641f3a71d3
priahitam <agapemanase27@gmail.com>
2024-06-12 21:44:49 +0700

    Belajar Laravel 11 | 9. Eloquent ORM & Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,40 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => '1',
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Agape Manase Sinuhaji',
-                'body'=> 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae aspernatur dolores, labore cum amet repellat minus, sed eveniet illo id illum qui ex enim optio aliquid suscipit provident pariatur cupiditate.',
-            ],
-            [
-                'id' => '2',
-                'title' => 'Judul Artikel 2',
-                'slug' => 'judul-artikel-2',
-                'author' => 'Agape Manase Sinuhaji',
-                'body'=> 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae aspernatur dolores, labore cum amet repellat minus, sed eveniet illo id illum qui ex enim optio aliquid suscipit provident pariatur cupiditate. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae aspernatur dolores, labore cum amet repellat minus, sed eveniet illo id illum qui ex enim optio aliquid suscipit provident pariatur cupiditate.',
-            ]
-        ];
-    }
+    use HasFactory;
 
-    public static function find($slug): array 
-    {
-
-
-        $post =  Arr::first(Post::all(), fn($post) => $post['slug'] == $slug);
-
-        if(! $post) {
-            abort(404);
-        }
-
-        return $post;
-    }
+    protected $fillable = [
+        'title',
+        'author',
+        'slug',
+        'body',
+    ];
 }
