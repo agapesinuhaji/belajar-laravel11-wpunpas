46334155ee2a996c9f6da8161421f9be5c593c68
priahitam <agapemanase27@gmail.com>
2024-06-23 19:47:48 +0700

    Belajar Laravel 11 | 11. Eloquent Relationship

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -18,8 +19,8 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => fake()->sentence(6),
-            'author' => fake()->name(),
+            'title' => fake()->sentence(),
+            'author_id' => User::factory(),
             'slug' => Str::slug(fake()->sentence()),
             'body' => fake()->text(),
         ];
